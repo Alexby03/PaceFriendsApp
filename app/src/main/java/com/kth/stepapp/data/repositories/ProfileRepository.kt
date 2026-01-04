8a7288c52190f4661a8a39f555d749c16761473d
Crazyicecat <noahjonsson35@gmail.com>
2026-01-04 17:37:24 +0100

    Change class to object

--- app/src/main/java/com/kth/stepapp/data/repositories/ProfileRepository.kt
+++ app/src/main/java/com/kth/stepapp/data/repositories/ProfileRepository.kt
@@ -3,7 +3,8 @@ package com.kth.stepapp.data.repository
 import android.util.Log
 import com.kth.stepapp.data.models.UserProfile
 
-class ProfileRepository {
+
+object ProfileRepository {
 
     fun saveProfile(profile: UserProfile) {
         // TODO: replace with real database call
